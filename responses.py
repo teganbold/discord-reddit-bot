871fde861e72f7372ea2d14aa18272ae978a2cce
Tegan Bold <tegan.bold@gmail.com>
2024-05-27 20:59:35 -0400

    Remove messages file

--- responses.py
+++
@@ -1,8 +0,0 @@
-def handle_response(message: str) -> str:
-    p_message = message.lower()
-
-    if p_message == "hello bewb":
-        return "Hey there Bewber"
-    
-    if p_message == "!help":
-        return "`Who knows, good luck.`"
